de5e3d6dabab8d3cecd497e8e729c4ded13a6cef
WillemdeKok <willem1995@hotmail.com>
2015-05-13 13:44:56 +0200

    OrderController now working

--- PhotoShop/src/main/java/com/photoshop/controllers/OrderController.java
+++ PhotoShop/src/main/java/com/photoshop/controllers/OrderController.java
@@ -5,16 +5,13 @@
  */
 package com.photoshop.controllers;
 
-import com.photoshop.models.IUser;
 import com.photoshop.models.UserType;
 import com.photoshop.models.admin.AdminDao;
 import com.photoshop.models.photographer.PhotographerDao;
 import com.photoshop.models.student.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,10 +51,12 @@ public class OrderController extends AbstractController  {
     
     @RequestMapping(value = "/orderoverzicht", method = RequestMethod.GET)
     public String Monitoring(ModelMap map, HttpServletRequest request) {
-        if (this.authenticate(UserType.STUDENT)) 
+        if (this.authenticate(UserType.ADMIN)) 
         {
+            System.out.println("Yay :)");
             return "order/orderoverzicht";
         }
+        System.out.println("Yay :)");
         return "redirect:../";
     }
     
