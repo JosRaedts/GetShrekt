7aa0693938f6df67a9b182ade047f70cdf9059d0
WillemdeKok <willem1995@hotmail.com>
2015-05-19 14:04:26 +0200

    modified ordersummary

--- PhotoShop/src/main/java/com/photoshop/controllers/OrderController.java
+++ PhotoShop/src/main/java/com/photoshop/controllers/OrderController.java
@@ -57,6 +57,7 @@ public class OrderController extends AbstractController  {
         if (this.authenticate(UserType.ADMIN)) 
         {
             map.put("orders", this.orderDao.getList());
+            map.put("studentdao", this.studentDao);
             System.out.println("Yay :)");
             return "order/orderoverzicht";
         }
