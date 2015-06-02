dbd0a02f54c70b32afc45ff9777ab5c39a16c372
Jos Raedts <josraedts_109@hotmail.com>
2015-06-02 15:22:46 +0200

    status toegevoegd aan detail pagina

--- PhotoShop/src/test/java/Tests/ModelTests.java
+++ PhotoShop/src/test/java/Tests/ModelTests.java
@@ -8,8 +8,14 @@ package Tests;
 import com.photoshop.misc.ImageManager;
 import com.photoshop.models.admin.Admin;
 import com.photoshop.models.admin.AdminDao;
+import com.photoshop.models.order.Order;
+import com.photoshop.models.order.OrderDao;
+import com.photoshop.models.photo.Photo;
+import com.photoshop.models.photo.PhotoDao;
 import com.photoshop.models.photographer.Photographer;
 import com.photoshop.models.photographer.PhotographerDao;
+import com.photoshop.models.product.Product;
+import com.photoshop.models.product.ProductDao;
 import com.photoshop.models.school.School;
 import com.photoshop.models.school.SchoolDao;
 import com.photoshop.models.schoolClass.SchoolClass;
@@ -19,6 +25,7 @@ import com.photoshop.models.student.StudentDao;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.sql.Date;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -37,7 +44,9 @@ public class ModelTests {
     Admin admin;
     School school;
     SchoolClass schoolClass;
-            
+    Photo photo;
+    Product product;
+    Order order;
     
     public ModelTests() {
     }
@@ -57,12 +66,18 @@ public class ModelTests {
         PhotographerDao photographerDao = new PhotographerDao();
         SchoolDao schoolDao = new SchoolDao();
         SchoolClassDao schoolClassDao = new SchoolClassDao();
+        PhotoDao photoDao = new PhotoDao();
+        ProductDao productDao = new ProductDao();
+        OrderDao orderDao = new OrderDao();
         
         this.student = studentDao.getById(2);
         this.photographer = photographerDao.getById(2);
         this.admin = adminDao.getById(1);
         this.school = schoolDao.getById(1);
         this.schoolClass = schoolClassDao.getById(1);
+        this.photo = photoDao.getById(1);
+        this.product = productDao.getById(1);
+        this.order = orderDao.getById(1);
     }
     
     @After
@@ -215,5 +230,101 @@ public class ModelTests {
         BufferedImage bufftestpicture = IM.resize(testpicture, 100, 100);
         System.out.println(bufftestpicture.getHeight());
         System.out.println(bufftestpicture.getWidth());
+    }
+    
+    @Test
+    public void testPhoto()
+    {
+        assertTrue("Photo should be active", photo.getActive());
+        assertEquals("Photo ID should be 1", 1, photo.getId());
+        assertEquals("Photo height must be 480", 480, photo.getHeight());
+        assertEquals("Photo width must be 640", 640, photo.getWidth());
+        assertEquals("ThumbnailURL should be thumb-1429614285504-schoolclass-1-1.jpg", "thumb-1429614285504-schoolclass-1-1.jpg", photo.getThumbnailURL());
+        assertEquals("LowResURL should be lowres-1429614285504-schoolclass-1-1.jpg", "lowres-1429614285504-schoolclass-1-1.jpg", photo.getLowResURL());
+        assertEquals("HighResURL should be 1429614285504-schoolclass-1-1.jpg", "1429614285504-schoolclass-1-1.jpg", photo.getHighResURL());
+        assertEquals("photographerid should be 4", 4, photo.getPhotographerID());
+        
+        photo.setId(25);
+        photo.setHeight(25);
+        photo.setWidth(256);
+        photo.setThumbnailURL("testphoto");
+        photo.setLowResURL("1235AC");
+        photo.setHighResURL("56789");
+        photo.setActive(false);
+        photo.setPhotographerID(12);
+        
+        assertFalse("Photo should be inactive", photo.getActive());
+        assertEquals("Photo ID should be 25", 25, photo.getId());
+        assertEquals("Photo height must be 25", 25, photo.getHeight());
+        assertEquals("Photo width must be 256", 256, photo.getWidth());
+        assertEquals("ThumbnailURL should be testphoto", "testphoto", photo.getThumbnailURL());
+        assertEquals("LowResURL should be 1235AC", "1235AC", photo.getLowResURL());
+        assertEquals("HighResURL should be 56789", "56789", photo.getHighResURL());
+        assertEquals("photographerid should be 12", 12, photo.getPhotographerID());
+                     
+        photo.setId(1);
+        photo.setHeight(480);
+        photo.setWidth(640);
+        photo.setThumbnailURL("thumb-1429614285504-schoolclass-1-1.jpg");
+        photo.setLowResURL("lowres-1429614285504-schoolclass-1-1.jpg");
+        photo.setHighResURL("1429614285504-schoolclass-1-1.jpg");
+        photo.setActive(true);
+        photo.setPhotographerID(4);
+        
+        assertTrue("Photo should be active", photo.getActive());
+        assertEquals("Photo ID should be 1", 1, photo.getId());
+        assertEquals("Photo height must be 480", 480, photo.getHeight());
+        assertEquals("Photo width must be 640", 640, photo.getWidth());
+        assertEquals("ThumbnailURL should be thumb-1429614285504-schoolclass-1-1.jpg", "thumb-1429614285504-schoolclass-1-1.jpg", photo.getThumbnailURL());
+        assertEquals("LowResURL should be lowres-1429614285504-schoolclass-1-1.jpg", "lowres-1429614285504-schoolclass-1-1.jpg", photo.getLowResURL());
+        assertEquals("HighResURL should be 1429614285504-schoolclass-1-1.jpg", "1429614285504-schoolclass-1-1.jpg", photo.getHighResURL());
+        assertEquals("photographerid should be 4", 4, photo.getPhotographerID());
+    }
+    
+    @Test
+    public void testProduct(){
+        assertEquals("Productid should be 1", 1, product.getId());
+        assertEquals("Productname should be Foto op fotopapier klein", "Foto op fotopapier klein", product.getName());
+        assertEquals("Height should be 10", 10, product.getHeight());
+        assertEquals("Width should be 15", 15, product.getWidth());
+        assertEquals("imageURL should be photo.png", "photo.png", product.getImageURL());
+        assertTrue("product should be active", product.getActive());
+        
+        product.setId(2);
+        product.setName("Foto");
+        product.setHeight(2);
+        product.setWidth(2);
+        product.setImageURL("Foto'tje");
+        product.setActive(false);
+        product.setPrice(new Double(300.0));
+        
+        assertEquals("Productid should be 2", 2, product.getId());
+        assertEquals("Productname should be Foto", "Foto", product.getName());
+        assertEquals("Height should be 2", 2, product.getHeight());
+        assertEquals("Width should be 2", 2, product.getWidth());
+        assertEquals("imageURL should be Foto'tje", "Foto'tje", product.getImageURL());
+        assertFalse("product should be inactive", product.getActive());
+        assertEquals("price should be 300.0", new Double(300.0), product.getPrice());
+        
+        product.setId(1);
+        product.setName("Foto op fotopapier klein");
+        product.setHeight(10);
+        product.setWidth(15);
+        product.setImageURL("photo.png");
+        product.setActive(true);
+        product.setPrice(new Double(2.95));
+        
+        assertEquals("Productid should be 1", 1, product.getId());
+        assertEquals("Productname should be Foto op fotopapier klein", "Foto op fotopapier klein", product.getName());
+        assertEquals("Height should be 10", 10, product.getHeight());
+        assertEquals("Width should be 15", 15, product.getWidth());
+        assertEquals("imageURL should be photo.png", "photo.png", product.getImageURL());
+        assertTrue("product should be active", product.getActive());
+        assertEquals("price should be 2.95", new Double(2.95), product.getPrice());
+    }
+    
+    @Test
+    public void orderTest(){
+        
     }
 }
