52acb2ad0dea53dc5f7672c461272f2c67ddb7be
WillemdeKok <willem1995@hotmail.com>
2015-06-03 10:28:50 +0200

    Mailserver met Factuur is functioneel

--- PhotoShop/src/main/java/com/photoshop/misc/Mailgenerator.java
+++ PhotoShop/src/main/java/com/photoshop/misc/Mailgenerator.java
@@ -8,14 +8,22 @@ package com.photoshop.misc;
 import com.photoshop.models.order.Order;
 import java.util.Date;
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import org.springframework.core.env.Environment;
 
 /**
  *
@@ -24,8 +32,12 @@ import javax.mail.internet.MimeMessage;
 public class Mailgenerator {
 
     private Order order;
-    
-    public void Sendmail(String OntvangerEmail,Order order) {
+    private BodyPart messageBodyPart;
+
+    private Environment env;
+
+    public void Sendmail(String OntvangerEmail, Order order, Environment env) {
+        this.env = env;
         this.order = order;
         final String username = "fotowinkelpts42@gmail.com";
         final String password = "Fotoshop";
@@ -45,19 +57,39 @@ public class Mailgenerator {
                 });
 
         try {
-
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("fotowinkelpts42@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(OntvangerEmail));
             message.setSubject("Factuur" + order.getId());
-            message.setText("Dear Willem de kok,"
-                    + "\n\n No spam to my email, please!");
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("Bedankt voor het plaatsen van uw bestelling, hier is uw factuur."
+                    + "\n \nThank you for placing your order, here is your invoice.");
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = env.getProperty("logo") + order.getFactuur() + ".pdf";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+
+            messageBodyPart.setFileName(order.getFactuur());
+            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
             Transport.send(message);
 
             System.out.println("Done");
 
         } catch (MessagingException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
 
         }
